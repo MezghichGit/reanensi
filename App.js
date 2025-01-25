597373c0bc5d43bd04c828234d731fee9e55ae3f
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-01-25 11:39:04 +0100

    exemple avec stack navigator

--- App.js
+++ App.js
@@ -1,3 +1,51 @@
+import * as React from 'react';
+import {Button,Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', { name: 'Jane' })
+      }
+    />
+  );
+};
+
+const ProfileScreen = ({ navigation, route }) => {
+
+  return (<><Text>This is {route.params.name}'s profile</Text><Button
+    title="Go homepage"
+    onPress={() => navigation.navigate('Home')} /></>);
+
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Welcome' }}
+      />
+      <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: 'Me' }} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+};
+
+export default App;
+
+
+
+
+
+/*
 import React, { useState } from "react";
 import {View,Image, FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
 
